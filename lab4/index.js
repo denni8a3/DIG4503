c1f44c6b42494f234b07c62776051880051a530d
denni8a3 <37115861+denni8a3@users.noreply.github.com>
2020-02-06 13:27:07 -0500

    Express

---
+++ lab4/index.js
@@ -0,0 +1,50 @@
+const Express = require("express");
+const App = Express();
+const port = 80;
+
+const pokemons = require("json-pokemon");
+const chalk = require("chalk");
+
+
+App.get("/id/:id",(req,res) => {
+
+    
+    let result = {"error": "can't find!"};
+
+    pokemons.forEach((value) => {
+        if (value.id == req.params.id) {
+            result = value;
+        }
+    });
+    if(result.error){
+        //invalid
+        console.log(chalk.red(req.path));
+    } else {
+        //valid
+        console.log(chalk.green(req.path));
+    }
+    res.send(result);
+} );
+
+App.get("/name/:name", (req,res) => {
+    let result = { "error": "can't find!" };
+
+    pokemons.forEach((value) => {
+        if (value.name == req.params.name) {
+            result = value;
+        }
+    });
+    if (result.error) {
+        //invalid
+        console.log(chalk.red(req.path));
+    } else {
+        //valid
+        console.log(chalk.green(req.path));
+    }
+    res.send(result);
+});
+  
+App.listen(port,() => {
+    console.log("server running");
+});
+
