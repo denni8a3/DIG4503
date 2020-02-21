45d5934fdbe44ac8324b86275082d186ee2e89f7
denni8a3 <37115861+denni8a3@users.noreply.github.com>
2020-02-20 22:19:18 -0500

    React

---
+++ lab6/client/src/components/IdSearch/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+class IdSearch extends React.Component {
+
+    pokeId(event) {
+
+        // Stop (prevent) the 'default' form action
+        event.preventDefault();
+
+        // Find the element with 'id="score"'
+        let element = document.querySelector("#id");
+
+        fetch("http://localhost/id/" + element.value)
+            .then((res) => {
+                // Parse the string into a JavaScript object and return it
+                return res.json();
+            })
+            .then((processed) => {
+
+                // Find the element with 'id="reportingArea"'
+                let reporting = document.querySelector("#reportingArea");
+
+                // Does the 'processed' object have a property called 'error'?
+                if (processed.error) {
+                    reporting.innerHTML = processed.error;
+                } else {
+                    reporting.innerHTML = processed.name;
+                }
+
+            });
+    }
+    render() {
+        return(
+            <div><h2>ID</h2>
+                <form onSubmit={this.pokeId}>
+                    <input id="id" type="text"/>
+                        <button>Submit</button>
+                 </form>
+                
+                
+            </div>
+        );
+    }
+}
+export default IdSearch;
---
+++ lab6/index.js
@@ -0,0 +1,53 @@
+const Express = require("express");
+const App = Express();
+const port = 80;
+const cors = require("cors");
+
+
+const pokemons = require("json-pokemon");
+const chalk = require("chalk");
+
+App.use(cors());
+
+App.get("/id/:id", (req, res) => {
+
+
+    let result = { "error": "can't find!" };
+
+    pokemons.forEach((value) => {
+        if (value.id == req.params.id) {
+            result = value;
+        }
+    });
+    if (result.error) {
+        //invalid
+        console.log(chalk.red(req.path));
+    } else {
+        //valid
+        console.log(chalk.green(req.path));
+    }
+    res.send(result);
+});
+
+App.get("/name/:name", (req, res) => {
+    let result = { "error": "can't find!" };
+
+    pokemons.forEach((value) => {
+        if (value.name == req.params.name) {
+            result = value;
+        }
+    });
+    if (result.error) {
+        //invalid
+        console.log(chalk.red(req.path));
+    } else {
+        //valid
+        console.log(chalk.green(req.path));
+    }
+    res.send(result);
+});
+
+App.listen(port, () => {
+    console.log("server running");
+});
+
